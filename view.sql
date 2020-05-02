83d7806eccfaa9ed6c68413ea82663dba2552b33
Yana <43292779+yana5k@users.noreply.github.com>
2020-05-02 19:53:39 +0300

    Delete view.sql

--- view.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE VIEW rs_view AS
-SELECT albuminfo.artist, albuminfo.album, albumgenre.genre,albuminfo.year
-FROM albuminfo
-JOIN AlbumGenre ON albuminfo.id_album = albumgenre.id_album
-JOIN genres ON albumgenre.genre = genres.genre;
